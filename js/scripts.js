0e0c411b917dbdc108d7d94a53bbb0625005f8d3
Brandon Vasquez <thatsjaskett@gmail.com>
2023-01-17 15:00:54 +0100

    Creato logica bottone next

--- js/scripts.js
+++ js/scripts.js
@@ -3,25 +3,30 @@ const gallery = document.getElementById('gallery');
 const prev = document.getElementById('prev');
 const next = document.getElementById('next');
 
+const data = document.querySelectorAll('#gallery img');
+
+let currentActiveIndex = 0;
+data[currentActiveIndex].classList.add('active');
+
 const images = [
     {
-        image: 'img/01.webp',
+        image: 'img/1.webp',
         title: 'Marvel\'s Spiderman Miles Morale',
         text: 'Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.',
     }, {
-        image: 'img/02.webp',
+        image: 'img/2.webp',
         title: 'Ratchet & Clank: Rift Apart',
         text: 'Go dimension-hopping with Ratchet and Clank as they take on an evil emperor from another reality.',
     }, {
-        image: 'img/03.webp',
+        image: 'img/3.webp',
         title: 'Fortnite',
         text: "Grab all of your friends and drop into Epic Games Fortnite, a massive 100 - player face - off that combines looting, crafting, shootouts and chaos.",
     }, {
-        image: 'img/04.webp',
+        image: 'img/4.webp',
         title: 'Stray',
         text: 'Lost, injured and alone, a stray cat must untangle an ancient mystery to escape a long-forgotten city',
     }, {
-        image: 'img/05.webp',
+        image: 'img/5.webp',
         title: "Marvel's Avengers",
         text: 'Marvel\'s Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.',
     }
@@ -29,10 +34,19 @@ const images = [
 
 console.log(images);
 
-for(let i = 1; i <= images.length; i++) {
-    let singleCard = images[i];
+next.addEventListener('click',
+    function() {
+        // Rimuovo la classe active
+        data[currentActiveIndex].classList.remove('active');
 
-    const singleCardElement = `<img class="active" src="img/${i}.webp" alt="">`;
+        // Incrementiamo per cambiare immagine
+        currentActiveIndex++;
 
-    gallery.innerHTML += singleCardElement;
-}
+        if(currentActiveIndex == images.length) {
+            currentActiveIndex = 0;
+        }
+
+        // Assegno la classe active alla foto successiva
+        data[currentActiveIndex].classList.add('active');
+    }
+);
